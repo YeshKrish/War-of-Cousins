7a6d9b16371c53e538cd83b44bbdf62105a77d4a
YeshKrish <yeshwinkrishna@gmail.com>
2023-06-25 17:40:29 +0530

    Photon networking

--- Assets/ConnectToServer.cs
+++ Assets/ConnectToServer.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
@@ -11,4 +12,14 @@ public class ConnectToServer : MonoBehaviour
         PhotonNetwork.ConnectUsingSettings();
     }
 
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
 }
